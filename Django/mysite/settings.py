7f36e7b0d1063f9b17bc99160543d86036bba7a6
nareshsutha28 <nareshsutha28@gmail.com>
2022-08-04 10:28:23 +0530

    hello

--- Django/mysite/settings.py
+++ Django/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i$vw#n2y_i(vac$s#y4^!ecxq4)!ounn)zs-0q!#fw6s2*e0+-'
+SECRET_KEY = 'django-insecure-o=ob&hq3#pvg&!j!yb9&@79p(r8c&759t%ojh47cek7r6as#0l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
